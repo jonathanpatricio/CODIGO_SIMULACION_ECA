9c367688680e89eee5ba7e21ac9e7677e3d6eb3e
jonathanpatricio <77943614+jonathanpatricio@users.noreply.github.com>
2022-10-23 21:21:14 -0500

    Update Similaciones.R

--- Similaciones.R
+++ Similaciones.R
@@ -295,7 +295,196 @@ var_cov_v0iv1i <- function(sd_v0i, sd_v1i, cor_v0iv1i) {
   return(list(var_v0i = var_v0i,  var_v1i = var_v1i, var_cov_v0iv1i = var_cov_v0iv1i))
 }
 
-# Escenario 10
+Comp_2_treat_missing <- function(yij_2_treat, sample_min, sample_max, repeticiones, t, k, m) {
+  
+  #Semilla
+  set.seed(16)
+  
+  #Librerías
+  library(dplyr)
+  library(gee)
+  library(ggplot2)
+  library(lmerTest)
+  library(lme4)
+  
+  library(survey)
+  library(sampling)
+
+  #Matrices
+  Gee_intercanbiable <- matrix(0,(sample_max-sample_min),repeticiones)
+  #Gee_AR1            <- matrix(0,(sample_max-sample_min),repeticiones)
+  Gee_unstructured   <- matrix(0,(sample_max-sample_min),repeticiones)
+  Mixto_intercepto   <- matrix(0,(sample_max-sample_min),repeticiones)
+  Mixto_pen_inter    <- matrix(0,(sample_max-sample_min),repeticiones)
+  
+  missing_treat_1    <- matrix(0,(sample_max-sample_min),repeticiones)
+  missing_treat_2    <- matrix(0,(sample_max-sample_min),repeticiones)
+  
+  #Bucle
+  for (i in (sample_min:sample_max)*k) {for(j in 1:repeticiones){
+    
+    sample_treat_1 <- yij_2_treat[sample(x = 1:(nrow(yij_2_treat)/2), size = i, replace = FALSE),]
+    
+    # Eliminando las observaciones paara simular las pérdidas de seguimiento para el tratamiento 1 
+    
+    t1 <- as.data.frame(cbind(ID = sample_treat_1$ID, t1 = sample_treat_1$V1) )
+    
+    t2 <- cbind(ID = sample_treat_1$ID, t2 = sample_treat_1$V2, umbral = cut(x = sample_treat_1$V2, breaks = c(-Inf, quantile(x = sample_treat_1$V2, probs = 0.10), quantile(x = sample_treat_1$V2, probs = 0.90),Inf))) 
+    t2_borrar <- sampling::strata(data = t2, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_1)*m/(t-1))*0.4), ceiling((nrow(sample_treat_1)*m/(t-1))*0.2), ceiling((nrow(sample_treat_1)*m/(t-1))*0.4)))[,2]
+    t2 <- as.data.frame(t2[-t2_borrar,] )
+    
+    t3 <- cbind(ID = sample_treat_1$ID, t3 = sample_treat_1$V3, umbral = cut(x = sample_treat_1$V3, breaks = c(-Inf, quantile(x = sample_treat_1$V3, probs = 0.10), quantile(x = sample_treat_1$V3, probs = 0.90),Inf))) 
+    t3_borrar <- sampling::strata(data = t3, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_1)*m/(t-1))*0.4), ceiling((nrow(sample_treat_1)*m/(t-1))*0.2), ceiling((nrow(sample_treat_1)*m/(t-1))*0.4)))[,2]
+    t3_borrar <- c(t2_borrar,t3_borrar)
+    t3 <- as.data.frame(t3[-t3_borrar,])
+    
+    t4 <- cbind(ID = sample_treat_1$ID, t4 = sample_treat_1$V4, umbral = cut(x = sample_treat_1$V4, breaks = c(-Inf, quantile(x = sample_treat_1$V4, probs = 0.10), quantile(x = sample_treat_1$V4, probs = 0.90),Inf))) 
+    t4_borrar <- sampling::strata(data = t4, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_1)*m/(t-1))*0.4), ceiling((nrow(sample_treat_1)*m/(t-1))*0.2), ceiling((nrow(sample_treat_1)*m/(t-1))*0.4)))[,2]
+    t4_borrar <- c(t3_borrar,t4_borrar)
+    t4 <- as.data.frame(t4[-t4_borrar,])  
+    
+    t5 <- cbind(ID = sample_treat_1$ID, t5 = sample_treat_1$V5, umbral = cut(x = sample_treat_1$V5, breaks = c(-Inf, quantile(x = sample_treat_1$V5, probs = 0.10), quantile(x = sample_treat_1$V5, probs = 0.90),Inf))) 
+    t5_borrar <- sampling::strata(data = t5, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_1)*m/(t-1))*0.4), ceiling((nrow(sample_treat_1)*m/(t-1))*0.2), ceiling((nrow(sample_treat_1)*m/(t-1))*0.4)))[,2]
+    t5_borrar <- c(t4_borrar,t5_borrar)
+    t5 <- as.data.frame(t5[-t5_borrar,])  
+    
+    t6 <- cbind(ID = sample_treat_1$ID, t6 = sample_treat_1$V6, umbral = cut(x = sample_treat_1$V6, breaks = c(-Inf, quantile(x = sample_treat_1$V6, probs = 0.10), quantile(x = sample_treat_1$V6, probs = 0.90),Inf))) 
+    t6_borrar <- sampling::strata(data = t6, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_1)*m/(t-1))*0.4), ceiling((nrow(sample_treat_1)*m/(t-1))*0.2), ceiling((nrow(sample_treat_1)*m/(t-1))*0.4)))[,2]
+    t6_borrar <- c(t5_borrar,t6_borrar)
+    t6 <- as.data.frame(t6[-t6_borrar,]) 
+    
+    t7 <- cbind(ID = sample_treat_1$ID, t7 = sample_treat_1$V7, umbral = cut(x = sample_treat_1$V7, breaks = c(-Inf, quantile(x = sample_treat_1$V7, probs = 0.10), quantile(x = sample_treat_1$V7, probs = 0.90),Inf))) 
+    t7_borrar <- sampling::strata(data = t7, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_1)*m/(t-1))*0.4), ceiling((nrow(sample_treat_1)*m/(t-1))*0.2), ceiling((nrow(sample_treat_1)*m/(t-1))*0.4)))[,2]
+    t7_borrar <- c(t6_borrar,t7_borrar)
+    t7 <- as.data.frame(t7[-t7_borrar,]) 
+    
+    t8 <- cbind(ID = sample_treat_1$ID, t8 = sample_treat_1$V8, umbral = cut(x = sample_treat_1$V8, breaks = c(-Inf, quantile(x = sample_treat_1$V8, probs = 0.10), quantile(x = sample_treat_1$V8, probs = 0.90),Inf))) 
+    t8_borrar <- sampling::strata(data = t8, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_1)*m/(t-1))*0.4), ceiling((nrow(sample_treat_1)*m/(t-1))*0.2), ceiling((nrow(sample_treat_1)*m/(t-1))*0.4)))[,2]
+    t8_borrar <- c(t7_borrar,t8_borrar)
+    t8 <- as.data.frame(t8[-t8_borrar,]) 
+    
+    sample_treat_1_perdidas <- full_join(x = t1,    y = t2, by = "ID")
+    sample_treat_1_perdidas <- full_join(x = sample_treat_1_perdidas, y = t3, by = "ID")
+    sample_treat_1_perdidas <- full_join(x = sample_treat_1_perdidas, y = t4, by = "ID")
+    sample_treat_1_perdidas <- full_join(x = sample_treat_1_perdidas, y = t5, by = "ID")
+    sample_treat_1_perdidas <- full_join(x = sample_treat_1_perdidas, y = t6, by = "ID")
+    sample_treat_1_perdidas <- full_join(x = sample_treat_1_perdidas, y = t7, by = "ID")
+    sample_treat_1_perdidas <- full_join(x = sample_treat_1_perdidas, y = t8, by = "ID")
+    sample_treat_1_perdidas <- mutate(sample_treat_1_perdidas, treat = rep(1, nrow(sample_treat_1)))
+    
+    sample_treat_1_perdidas <- sample_treat_1_perdidas[,-c(4,6,8,10,12,14,16)]
+    
+    missing_treat_1[(i/k)-(sample_min),j]    <- length(t8_borrar)/nrow(sample_treat_1_perdidas)
+    
+    # Eliminando las observaciones paara simular las pérdidas de seguimiento para el tratamiento 2
+    
+    sample_treat_2 <- yij_2_treat[sample(x = (nrow(yij_2_treat)/2+1):nrow(yij_2_treat), size = i, replace = FALSE),]
+    
+    t1 <- as.data.frame(cbind(ID = sample_treat_2$ID, t1 = sample_treat_2$V1) )
+    
+    t2 <- cbind(ID = sample_treat_2$ID, t2 = sample_treat_2$V2, umbral = cut(x = sample_treat_2$V2, breaks = c(-Inf, quantile(x = sample_treat_2$V2, probs = 0.10), quantile(x = sample_treat_2$V2, probs = 0.90),Inf))) 
+    t2_borrar <- sampling::strata(data = t2, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_2)*m/(t-1))*0.4), ceiling((nrow(sample_treat_2)*m/(t-1))*0.2), ceiling((nrow(sample_treat_2)*m/(t-1))*0.4)))[,2]
+    t2 <- as.data.frame(t2[-t2_borrar,] )
+    
+    t3 <- cbind(ID = sample_treat_2$ID, t3 = sample_treat_2$V3, umbral = cut(x = sample_treat_2$V3, breaks = c(-Inf, quantile(x = sample_treat_2$V3, probs = 0.10), quantile(x = sample_treat_2$V3, probs = 0.90),Inf))) 
+    t3_borrar <- sampling::strata(data = t3, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_2)*m/(t-1))*0.4), ceiling((nrow(sample_treat_2)*m/(t-1))*0.2), ceiling((nrow(sample_treat_2)*m/(t-1))*0.4)))[,2]
+    t3_borrar <- c(t2_borrar,t3_borrar)
+    t3 <- as.data.frame(t3[-t3_borrar,])
+    
+    t4 <- cbind(ID = sample_treat_2$ID, t4 = sample_treat_2$V4, umbral = cut(x = sample_treat_2$V4, breaks = c(-Inf, quantile(x = sample_treat_2$V4, probs = 0.10), quantile(x = sample_treat_2$V4, probs = 0.90),Inf))) 
+    t4_borrar <- sampling::strata(data = t4, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_2)*m/(t-1))*0.4), ceiling((nrow(sample_treat_2)*m/(t-1))*0.2), ceiling((nrow(sample_treat_2)*m/(t-1))*0.4)))[,2]
+    t4_borrar <- c(t3_borrar,t4_borrar)
+    t4 <- as.data.frame(t4[-t4_borrar,])  
+    
+    t5 <- cbind(ID = sample_treat_2$ID, t5 = sample_treat_2$V5, umbral = cut(x = sample_treat_2$V5, breaks = c(-Inf, quantile(x = sample_treat_2$V5, probs = 0.10), quantile(x = sample_treat_2$V5, probs = 0.90),Inf))) 
+    t5_borrar <- sampling::strata(data = t5, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_2)*m/(t-1))*0.4), ceiling((nrow(sample_treat_2)*m/(t-1))*0.2), ceiling((nrow(sample_treat_2)*m/(t-1))*0.4)))[,2]
+    t5_borrar <- c(t4_borrar,t5_borrar)
+    t5 <- as.data.frame(t5[-t5_borrar,])  
+    
+    t6 <- cbind(ID = sample_treat_2$ID, t6 = sample_treat_2$V6, umbral = cut(x = sample_treat_2$V6, breaks = c(-Inf, quantile(x = sample_treat_2$V6, probs = 0.10), quantile(x = sample_treat_2$V6, probs = 0.90),Inf))) 
+    t6_borrar <- sampling::strata(data = t6, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_2)*m/(t-1))*0.4), ceiling((nrow(sample_treat_2)*m/(t-1))*0.2), ceiling((nrow(sample_treat_2)*m/(t-1))*0.4)))[,2]
+    t6_borrar <- c(t5_borrar,t6_borrar)
+    t6 <- as.data.frame(t6[-t6_borrar,]) 
+    
+    t7 <- cbind(ID = sample_treat_2$ID, t7 = sample_treat_2$V7, umbral = cut(x = sample_treat_2$V7, breaks = c(-Inf, quantile(x = sample_treat_2$V7, probs = 0.10), quantile(x = sample_treat_2$V7, probs = 0.90),Inf))) 
+    t7_borrar <- sampling::strata(data = t7, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_2)*m/(t-1))*0.4), ceiling((nrow(sample_treat_2)*m/(t-1))*0.2), ceiling((nrow(sample_treat_2)*m/(t-1))*0.4)))[,2]
+    t7_borrar <- c(t6_borrar,t7_borrar)
+    t7 <- as.data.frame(t7[-t7_borrar,]) 
+    
+    t8 <- cbind(ID = sample_treat_2$ID, t8 = sample_treat_2$V8, umbral = cut(x = sample_treat_2$V8, breaks = c(-Inf, quantile(x = sample_treat_2$V8, probs = 0.10), quantile(x = sample_treat_2$V8, probs = 0.90),Inf))) 
+    t8_borrar <- sampling::strata(data = t8, method = c("srswor"), stratanames = c("umbral"), size = c( ceiling((nrow(sample_treat_2)*m/(t-1))*0.4), ceiling((nrow(sample_treat_2)*m/(t-1))*0.2), ceiling((nrow(sample_treat_2)*m/(t-1))*0.4)))[,2]
+    t8_borrar <- c(t7_borrar,t8_borrar)
+    t8 <- as.data.frame(t8[-t8_borrar,]) 
+    
+    sample_treat_2_perdidas <- full_join(x = t1,    y = t2, by = "ID")
+    sample_treat_2_perdidas <- full_join(x = sample_treat_2_perdidas, y = t3, by = "ID")
+    sample_treat_2_perdidas <- full_join(x = sample_treat_2_perdidas, y = t4, by = "ID")
+    sample_treat_2_perdidas <- full_join(x = sample_treat_2_perdidas, y = t5, by = "ID")
+    sample_treat_2_perdidas <- full_join(x = sample_treat_2_perdidas, y = t6, by = "ID")
+    sample_treat_2_perdidas <- full_join(x = sample_treat_2_perdidas, y = t7, by = "ID")
+    sample_treat_2_perdidas <- full_join(x = sample_treat_2_perdidas, y = t8, by = "ID")
+    sample_treat_2_perdidas <- mutate(sample_treat_2_perdidas, treat = rep(2, nrow(sample_treat_2_perdidas)))
+    
+    sample_treat_2_perdidas <- sample_treat_2_perdidas[,-c(4,6,8,10,12,14,16)]  
+    
+    missing_treat_2[(i/k)-(sample_min),j]    <- length(t8_borrar)/nrow(sample_treat_2_perdidas)
+    
+    # Uniendo las bases de los tratamientos con pérdidas de seguimiento
+    
+    sample <- bind_rows(sample_treat_1_perdidas,sample_treat_2_perdidas)
+    
+    # Pasando la base a formato ancho
+    
+    sample_long <- reshape(data = sample,varying = 2:(t+1), v.names = "yij", timevar= "tiempo", idvar = "ID", direction = "long")
+    sample_long <- arrange(sample_long,ID,tiempo)
+    sample_long$tiempo <- as.numeric(sample_long$tiempo)
+    sample_long$tiempo <- (sample_long$tiempo-1)/(t-1)
+    
+    #Modelos
+    intercanbiable <- gee(yij ~ treat + tiempo + treat * tiempo, id = ID, data = sample_long, family = gaussian, corstr = "exchangeable")
+    #AR1            <- gee(yij ~ treat + tiempo + treat * tiempo, id = ID, data = sample_long, family = gaussian, corstr = "AR-M", Mv = 1)
+    unstructured   <- gee(yij ~ treat + tiempo + treat * tiempo, id = ID, data = sample_long, family = gaussian, corstr = "unstructured")
+    intercepto     <- lmer(yij ~ treat + tiempo + treat * tiempo + (1|ID), data = sample_long, REML = FALSE)
+    pen_intercepto <- lmer(yij ~ treat + tiempo + treat * tiempo + (tiempo|ID), data = sample_long, REML = FALSE)
+    
+    #Completando las matrices con la decisión de la hipótesis
+    Gee_intercanbiable[(i/k)-(sample_min),j] <-if(pnorm(as.matrix(intercanbiable$coefficients)[4,]/sqrt(intercanbiable$robust.variance[4,4]), 0, 1)        < 0.05) 1 else 0
+    #Gee_AR1[(i/k)-(sample_min),j]            <-if(pnorm(as.matrix(AR1$coefficients)[4,]/sqrt(AR1$robust.variance[4,4]), 0, 1)                              < 0.05) 1 else 0
+    Gee_unstructured[(i/k)-(sample_min),j]   <-if(pnorm(as.matrix(unstructured$coefficients)[4,]/sqrt(unstructured$robust.variance[4,4]), 0, 1)            < 0.05) 1 else 0
+    Mixto_intercepto[(i/k)-(sample_min),j]   <-if(pt(as.matrix(intercepto@beta)[4,]/sqrt(intercepto@vcov_beta[4,4]), df = as.matrix(intercepto@Gp)[2,]-1)     < 0.05) 1 else 0
+    Mixto_pen_inter[(i/k)-(sample_min),j]    <-if(pt(as.matrix(pen_intercepto@beta)[4,]/sqrt(pen_intercepto@vcov_beta[4,4]), df = as.matrix(pen_intercepto@Gp)[2,]-1)     < 0.05) 1 else 0
+    
+    
+    
+    
+  }}
+  
+  #Base de datos
+  Gee_inter  <- as.matrix(apply(X = Gee_intercanbiable, MARGIN = 1, FUN = mean))
+  #Gee_AR     <- as.matrix(apply(X = Gee_AR1,            MARGIN = 1, FUN = mean))
+  Gee_unst   <- as.matrix(apply(X = Gee_unstructured,   MARGIN = 1, FUN = mean))
+  Mixto_inte <- as.matrix(apply(X = Mixto_intercepto,   MARGIN = 1, FUN = mean))
+  Mixto_pen_ <- as.matrix(apply(X = Mixto_pen_inter,    MARGIN = 1, FUN = mean))
+  
+  missing_treat__1 <- as.matrix(apply(X = missing_treat_1,    MARGIN = 1, FUN = mean))
+  missing_treat__2 <- as.matrix(apply(X = missing_treat_2,    MARGIN = 1, FUN = mean))
+  
+  Base <- as.data.frame(cbind(ID = (sample_min:(sample_max-1)*k)))
+  Base <- mutate(Base,Gee_inter)
+  #Base <- mutate(Base,Gee_AR)
+  Base <- mutate(Base,Gee_unst)
+  Base <- mutate(Base,Mixto_inte)
+  Base <- mutate(Base,missing_treat__1)
+  Base <- mutate(Base,missing_treat__2)
+  
+  
+  return(Base = Base)
+  
+}
+                                        
+                                        
+
+# Escenario 1
 var_cov_v0iv1i(sd_v0i = 10.1, sd_v1i = 8, cor_v0iv1i = -0.75)
 
 # Población
@@ -320,14 +509,7 @@ yij_3_treat <- población$Treat_3$yij_3
 # Escenario
 Escenario <- Comp_2_treat(yij_2_treat = yij_2_treat, sample_min = 10, sample_max = 20, repeticiones = 10, t = 4, k = 1)
 
-# Escenario_10.1
+# Escenario
 Escenario <- Comp_3_treat(yij_2_treat = yij_2_treat, sample_min = 10, sample_max = 20, repeticiones = 10, t = 4, k = 1)
-
-write.table(Escenario_10.1$Base, "Datos/Escenario 10/Base.txt")
-write.table(Escenario_10.1$Gee_intercanbiable, "Datos/Escenario 10/Gee_intercanbiable.txt")
-write.table(Escenario_10.1$Gee_AR1, "Datos/Escenario 10/Gee_AR1.txt")
-write.table(Escenario_10.1$Gee_unstructured, "Datos/Escenario 10/Gee_unstructured.txt")
-write.table(Escenario_10.1$Mixto_intercepto, "Datos/Escenario 10/Mixto_intercepto.txt")
-write.table(Escenario_10.1$Mixto_pen_inter, "Datos/Escenario 10/Mixto_pen_inter.txt")
                                         
                                         
